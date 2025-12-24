bcd5544873afafc04b8667609c003694f9a5333e
Domagoj Skoko <karlsgamba@gmail.com>
2025-12-24 19:43:20 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+const repoName = "<crafted-spaces>";
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? `/${repoName}/` : "/",
   server: {
     host: "::",
     port: 8080,
